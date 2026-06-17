fa8f74a9fed1007c316e5895d9092b585a0fe074
unknown <varunak2004@gmail.com>
2026-06-17 13:27:52 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = 8000;
 const app = express();
+const connectionUrl = "mongodb://localhost:27017/TODODb";
+mongoose.connect(connectionUrl).then(() => console.log("Database connected successfully")).catch((error) => console.log(error.message));
 app.set("view engine", "ejs");
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
